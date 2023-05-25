720f2469835fb1c42a28888a8b96fd23e5f554d7
priyank <priyankgandhi000@gmail.com>
2023-05-25 18:03:26 +0530

    ui_development

--- app/src/main/java/com/example/reminisce_ticketing/MainActivity.java
+++ app/src/main/java/com/example/reminisce_ticketing/MainActivity.java
@@ -1,29 +1,15 @@
 package com.example.reminisce_ticketing;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
-
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-
 import android.view.View;
-import com.example.reminisce_ticketing.apiservice.ApiInterface;
-import com.example.reminisce_ticketing.apiservice.RetrofitClient;
-import com.example.reminisce_ticketing.auth.login.LoginActivity;
-import com.example.reminisce_ticketing.auth.login.LoginViewModel;
 import com.example.reminisce_ticketing.constant.Constants;
 import com.example.reminisce_ticketing.databinding.ActivityMainBinding;
 import com.example.reminisce_ticketing.factory.GeneralViewModelFactory;
 import com.example.reminisce_ticketing.ui.change_password.ChangePassword;
 import com.example.reminisce_ticketing.ui.home.HomeFragment;
-import com.example.reminisce_ticketing.ui.notification.Notification;
 import com.example.reminisce_ticketing.ui.scan_with_camera.ScanWithCamera;
 
 
@@ -42,8 +28,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
-
     private void initView() {
         findViewById(R.id.menuButton).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,13 +35,13 @@ public class MainActivity extends AppCompatActivity {
                 binding.drawerLayout.open();
             }
         });
-        findViewById(R.id.iv_notification).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Notification.class);
-                startActivity(intent);
-            }
-        });
+//        findViewById(R.id.iv_notification).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, Notification.class);
+//                startActivity(intent);
+//            }
+//        });
         findViewById(R.id.home_item).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
